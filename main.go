45b1d78e3ce9dd677ef7e27522f73a88f2ad7d3d
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-10-09 13:09:38 +0700

    chore: clean up back-end code

--- main.go
+++ main.go
@@ -22,45 +22,42 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page")
 }
 
-// define a reader which will listen for
-// new messages being sent to our WebSocket
-// endpoint
+// reader will continuously listen for new messages being sent to the websocket endpoint
 func reader(conn *websocket.Conn) {
 	go func() {
 		for {
 			// read in a message
-			_, p, err := conn.ReadMessage()
+			_, msg, err := conn.ReadMessage()
 			if err != nil {
 				log.Println(err)
 				return
 			}
-			// print out that message for clarity
-			fmt.Println(string(p))
-			commands := strings.Split(string(p), " ")
+
+			// print out the message for debugging
+			fmt.Println(string(msg))
+
+			// Get commands to be executed from the front-end
+			commands := strings.Split(string(msg), " ")
 			if len(commands) < 1 {
 				log.Println("Empty command...")
 				return
 			}
 
+			// Execute the command in its own pty (pseudo-terminal)
 			c := exec.Command("websh", commands[1:]...)
 			f, err := pty.Start(c)
 			if err != nil {
 				panic(err)
 			}
+
+			// Send the output of the command from pty back to the front-end
 			var buf bytes.Buffer
 			io.Copy(&buf, f)
-			// resp := fmt.Sprintf("\n\n%s", string(buf.Bytes()))
 			err = conn.WriteMessage(1, buf.Bytes())
 			if err != nil {
 				log.Println(err)
 				return
 			}
-
-			// if err := conn.WriteMessage(messageType, p); err != nil {
-			// 	log.Println(err)
-			// 	return
-			// }
-
 		}
 	}()
 }
@@ -68,20 +65,13 @@ func reader(conn *websocket.Conn) {
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-	// upgrade this connection to a WebSocket
-	// connection
+	// upgrade this connection to a websocket connection
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 	}
 
 	log.Println("Client Connected")
-	// err = ws.WriteMessage(1, []byte("Hi Client!"))
-	// if err != nil {
-	// 				log.Println(err)
-	// }
-	// listen indefinitely for new messages coming
-	// through on our WebSocket connection
 	reader(ws)
 }
 
