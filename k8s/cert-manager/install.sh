8888a57ca9e90c5cc2ae49a2001866bfd1f8cccb
Ray Krishardi Layadi <raykrishardi@gmail.com>
2022-10-16 08:26:10 +0700

    feat: add https and wss support using cert-manager k8s

---
+++ k8s/cert-manager/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.9.1 \
+  --set installCRDs=true
+
+
+# clusterissuer
+kubectl apply -f clusterissuer-staging.yaml
+kubectl apply -f clusterissuer-production.yaml
